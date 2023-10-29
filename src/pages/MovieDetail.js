4d5034f3e8fa3e49eb6c3e690a6de957aa48d59d
Deepak <deepaksaraswat238@gmail.com>
2023-10-29 09:58:21 +0530

    'Updated'

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react"
-import { useParams, Link } from "react-router-dom"
-import useTitle from "../hook/useTitle"
+import { useParams } from "react-router-dom"
 import imageNotFound from '../assets/imageNotFound.jpg'
 
 export const MovieDetail = () => {
